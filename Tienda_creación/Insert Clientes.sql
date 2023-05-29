3ded8e783551f84c8cae951b814f4a8c55393c4d
InmaGalvez99 <128999618+InmaGalvez99@users.noreply.github.com>
2023-05-29 19:53:37 +0200

    Add files via upload
    Proyecto tienda

---
+++ Tienda_creación/Insert Clientes.sql
@@ -0,0 +1,89 @@
+USE [Facturas2.0]
+
+INSERT INTO Cliente
+VALUES
+('123456789A', 'Carlos', 'carlos@surname.com',23, 'Calle Notelodigo')
+
+INSERT INTO Cliente
+(dni, nombre)
+VALUES
+('111111111B', 'Juan')
+
+INSERT INTO Cliente
+VALUES
+('222222222C','Óscar','oscar@sumail.com',28,'Calle Sur nº 18')
+
+INSERT INTO Cliente
+VALUES
+('333333333C','Antonio','antonio@unmailmas.com',17,'Calle Oeste nº 21')
+
+INSERT INTO Cliente
+VALUES
+('444444444C','Jorge','jorge@sumail.com',36,'Calle mayor nº 5')
+
+INSERT INTO Cliente
+VALUES
+('555555555C','Jorge','jorge@otromail.com',27,'Calle menor nº 2 tercero')
+
+INSERT INTO Cliente
+VALUES
+('666666666C','Faustino','fa@sumail.com',47,'Avenida Principal nº 218')
+
+INSERT INTO Cliente
+VALUES
+('777777777D','Julio','julio@mejormail.com',26,'Calle del Poligono nº 26')
+
+INSERT INTO Cliente
+VALUES
+('888888888E','Susana','susan@susanmail.com',21,'Calle diagonal nº 82 A')
+
+INSERT INTO Cliente
+VALUES
+('999999999F','Esther','esther@thermail.com',28,'Calle Arriba nº 44 Segundo')
+
+INSERT INTO Cliente
+VALUES
+('111112222G','Sandra','sandra@mailsan.com',24,'Calle Abajo nº 18')
+
+INSERT INTO Cliente
+VALUES
+('222223333H','Silvia','silvia@mejormail.com',22,'Calle Sur nº 78 Cuarto')
+
+INSERT INTO Cliente
+VALUES
+('333334444I','Cristina','cris@mejormail.com',32,'Calle Abajo nº 2 Bajo')
+
+INSERT INTO Cliente
+VALUES
+('444455555I','Bea','bea@sumail.com',52,'Calle Abajo nº 31')
+
+INSERT INTO Cliente
+VALUES
+('555556666I','Yolanda','yoli@masmail.com',46,'Avenida extraradio nº 4')
+
+INSERT INTO Cliente
+VALUES
+('666667777J','Alicia','Ali@otromail.com',42,'Plaza Cuadrada nº 1')
+
+INSERT INTO Cliente
+VALUES
+('777776666I','Mar','mar@mejormail.com',18,'Calle diagonal nº 14 B')
+
+INSERT INTO Cliente
+VALUES
+('777778888M','Noelia','noe@otromailmas.com',52,'Calle montaña nº 2')
+
+INSERT INTO Cliente
+(dni,nombre,email)
+VALUES
+('888889999X','Lucas Pérez','lucas.perez@masmail.com')
+
+INSERT INTO Cliente
+VALUES
+('999900000Y','Santi Santos','santos.santi@gmail.com',47,'13,Rue del Percebe')
+
+SELECT *FROM Cliente
+
+
+
+
